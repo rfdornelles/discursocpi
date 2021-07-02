7ff9ff3d35775b2dbbefc5693b9320e8e712ae64
Rodrigo Dornelles <rodornelles@gmail.com>
2021-07-02 20:32:08 -0300

    função que organiza tabela com informações das sessões

--- R/ler_arquivos.R
+++ R/ler_arquivos.R
@@ -12,35 +12,94 @@ library(tibble)
 # lista de arquivos -------------------------------------------------------
 
 lista_html <- fs::dir_info("data-raw", regex = "\\.html$") %>%
-  dplyr::select(path, size) %>%
-  dplyr::arrange(size) %>%
   dplyr::pull(path)
 
-# raspar arquivo ----------------------------------------------------------
 
-arquivo <- lista_html[20]
+# função dados da sessões -------------------------------------------------
 
-data_sessao <- xml2::read_html(arquivo, encoding = "UTF-8") %>%
-  xml2::xml_find_all(
-    "//div[@class = 'escriba-jq']/*/div[@style = 'width:85%; float:left;']"
-  ) %>%
-  xml2::xml_text() %>%
-  stringr::str_trim()
+ler_infos_sessao <- function(arquivo) {
 
-data <- data_sessao %>%
-  stringr::str_extract("\\d{2,2}/\\d{2,2}/\\d{4,4}") %>%
-  lubridate::dmy()
+  # ler o html do arquivo, com encoding correto
+  html <- xml2::read_html(arquivo, encoding = "UTF-8")
 
-sessao <- data_sessao %>%
-  stringr::str_extract("(?<= - )\\d{1,3}")
+  # ler as informações do cabeçalho da página
+  infos_data_sessao <- html %>%
+    xml2::xml_find_all(
+      "//div[@class = 'escriba-jq']/*/div[@style = 'width:85%; float:left;']"
+    ) %>%
+    xml2::xml_text() %>%
+    stringr::str_trim()
 
-encerramento <- xml2::read_html(arquivo, encoding = "UTF-8") %>%
-  xml2::xml_find_all("//div[@class = 'anotacaoAberturaStyle']/span") %>%
-  xml2::xml_text() %>%
-  stringr::str_c(collapse = "") %>%
-  stringr::str_extract("(?<=encerrada..s) .*") %>%
-  stringr::str_extract_all("\\d+", simplify = T) %>%
-  stringr::str_c(collapse = ":")
+  # data da sessão
+  data <- infos_data_sessao %>%
+    stringr::str_extract("\\d{2,2}/\\d{2,2}/\\d{4,4}")
+
+
+  # número da sessão
+  sessao <- infos_data_sessao %>%
+    stringr::str_extract("(?<= - )\\d{1,3}") %>%
+    stringr::str_pad(width = 2, side = "left", pad = 0)
+
+  # pegar os dados do rodapé
+
+  # horário da abertura
+  abertura <- xml2::read_html(arquivo, encoding = "UTF-8") %>%
+    xml2::xml_find_all("//div[@class = 'anotacaoAberturaStyle']/span") %>%
+    xml2::xml_text() %>%
+    data.frame() %>%
+    dplyr::filter(stringr::str_detect(., pattern = "^\\(Iniciada")) %>%
+    as.character() %>%
+    stringr::str_extract("(?<=Iniciada..s) .*") %>%
+    stringr::str_extract_all("\\d+.*,") %>%
+    stringr::str_extract_all("\\d+", simplify = T) %>%
+    stringr::str_c(collapse = ":")
+
+  # colocar em formato date
+  abertura_completa <- paste(data, abertura) %>%
+    lubridate::dmy_hm()
+
+  # dados do encerramento
+  encerramento <- xml2::read_html(arquivo, encoding = "UTF-8") %>%
+    xml2::xml_find_all("//div[@class = 'anotacaoAberturaStyle']/span") %>%
+    xml2::xml_text() %>%
+    stringr::str_c(collapse = "") %>%
+    stringr::str_extract("(?<=encerrada..s) .*") %>%
+    stringr::str_extract_all("\\d+", simplify = T) %>%
+    stringr::str_c(collapse = ":")
+
+  # colocar no formato date
+  encerramento_completo <- paste(data, encerramento) %>%
+    lubridate::dmy_hm()
+
+  # gerar saída
+  tibble::tibble_row(
+    numero_sessao = sessao,
+    data_sessao = lubridate::dmy(data),
+    horario_abertura_sessao = abertura_completa,
+    horario_encerramento_sessao = encerramento_completo
+  )
+
+}
+
+
+# função limpar a sessão --------------------------------------------------
+
+limpar_discursos_sessao <- function(arquivo) {
+
+  # ler o html do arquivo, com encoding correto
+  html <- xml2::read_html(arquivo, encoding = "UTF-8")
+
+}
+
+
+
+
+
+
+
+
+
+
 
 discursos <- xml2::read_html(arquivo, encoding = "UTF-8") %>%
   xml2::xml_find_all("//table[@id = 'tabelaQuartos']") %>%
@@ -90,7 +149,14 @@ discursos_limpo %>%
 
 discursos_limpo %>%
   dplyr::select(horario, texto) %>%
-  # (O|A) (SR|SRA)\\.
   tidyr::separate_rows(texto, sep = "(O|A) (SR|SRA)\\.") %>%
   tidyr::separate(col = texto, into = c("orador", "texto_2"),
                   sep = " - ", remove = FALSE) %>% View()
+
+#######
+purrr::map_dfr(
+  lista_html,
+  ler_infos_sessao
+) %>%
+  dplyr::arrange(numero_sessao)
+
