a99929e8b4525899f6414f6751d4f1d09f17cf3c
Rodrigo Dornelles <rodornelles@gmail.com>
2021-07-04 20:42:24 -0300

    colocada função de tokenizar nas auxiliares

--- data-raw/exporta_base_tokenizada.R
+++ data-raw/exporta_base_tokenizada.R
@@ -18,36 +18,36 @@ data("discursos_cpi")
 base <- discursos_cpi %>%
   # selecionar colunas pertinentes
   dplyr::select(
-    numero_sessao:texto, partido_sigla, genero, senado
+    numero_sessao:data_sessao, partido_sigla, genero, senado
   )
 
-
+base_tokenizada <- tokenizar_base(base)
 # limpeza  ----------------------------------------------------------------
-
-base_tokenizada <- base %>%
-  tidytext::unnest_tokens(termo, texto) %>%
-  # remover acentos
-  dplyr::mutate(termo = abjutils::rm_accent(termo))
-
-# remover letras soltas
-base_tokenizada <- base_tokenizada %>%
-  dplyr::mutate(tamanho = stringr::str_length(termo)) %>%
-  dplyr::arrange(tamanho) %>%
-  dplyr::filter(tamanho > 1) %>%
-  dplyr::select(-tamanho)
-
-# stopwords
-stopwords <- c(tm::stopwords("pt"), "v", "exa", "sra", "sr", "ai", "sa",
-               "dr", "dra", "la", "aqui", "senhor", "senhora",
-               "entao", "neste", "nesta", "nesse", "nessa", "nisso",
-               "senador", "senadora", "excelencia", "senhoria",
-               "porque", "por", "que")
-
-# remover stopwords
-base_tokenizada <- base_tokenizada %>%
-  dplyr::filter(
-    !termo %in% abjutils::rm_accent(stopwords)
-  )
+#
+# base_tokenizada <- base %>%
+#   tidytext::unnest_tokens(termo, texto) %>%
+#   # remover acentos
+#   dplyr::mutate(termo = abjutils::rm_accent(termo))
+#
+# # remover letras soltas
+# base_tokenizada <- base_tokenizada %>%
+#   dplyr::mutate(tamanho = stringr::str_length(termo)) %>%
+#   dplyr::arrange(tamanho) %>%
+#   dplyr::filter(tamanho > 1) %>%
+#   dplyr::select(-tamanho)
+#
+# # stopwords
+# stopwords <- c(tm::stopwords("pt"), "v", "exa", "sra", "sr", "ai", "sa",
+#                "dr", "dra", "la", "aqui", "senhor", "senhora",
+#                "entao", "neste", "nesta", "nesse", "nessa", "nisso",
+#                "senador", "senadora", "excelencia", "senhoria",
+#                "porque", "por", "que")
+#
+# # remover stopwords
+# base_tokenizada <- base_tokenizada %>%
+#   dplyr::filter(
+#     !termo %in% abjutils::rm_accent(stopwords)
+#   )
 
 
 # exportar ----------------------------------------------------------------
