73bcecdfa11f1d479b48030ee7100bf79155c0b6
Rodrigo Dornelles <rodornelles@gmail.com>
2021-07-04 20:43:18 -0300

    função de tokenizar generalizada no script de funções auxiliares

--- R/auxiliares.R
+++ R/auxiliares.R
@@ -195,7 +195,7 @@ graficos_tempo_de_fala <- function (seletor_grafico_tempo_fala) {
 
 # ranking -----------------------------------------------------------------
 
-ranking_palavras_discurso <-function(
+ranking_palavras_discurso <- function(
   base = base_tokenizada,
   documento = "falante",
   ranking = 5,
@@ -258,7 +258,37 @@ ranking_palavras_discurso <-function(
 
   }
 
-
-
 }
 
+
+# tokenizar ---------------------------------------------------------------
+
+tokenizar_base <- function(base) {
+
+  base_tokenizada <- base %>%
+    tidytext::unnest_tokens(termo, texto) %>%
+    # remover acentos
+    dplyr::mutate(termo = abjutils::rm_accent(termo))
+
+  # remover letras soltas
+  base_tokenizada <- base_tokenizada %>%
+    dplyr::mutate(tamanho = stringr::str_length(termo)) %>%
+    dplyr::arrange(tamanho) %>%
+    dplyr::filter(tamanho > 1) %>%
+    dplyr::select(-tamanho)
+
+  # stopwords
+  stopwords <- c(tm::stopwords("pt"), "v", "exa", "sra", "sr", "ai", "sa",
+                 "dr", "dra", "la", "aqui", "senhor", "senhora",
+                 "entao", "neste", "nesta", "nesse", "nessa", "nisso",
+                 "senador", "senadora", "excelencia", "senhoria",
+                 "porque", "por", "que")
+
+  # remover stopwords
+  base_tokenizada <- base_tokenizada %>%
+    dplyr::filter(
+      !termo %in% abjutils::rm_accent(stopwords)
+    )
+
+  return(base_tokenizada)
+}
