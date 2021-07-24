32648a2b3b11d4c6aeaf57db41ff1590982496f7
Rodrigo Dornelles <31113760+rfdornelles@users.noreply.github.com>
2021-07-24 20:57:35 -0300

    Testando atualização com gh actions

---
+++ data-raw/update-data.R
@@ -0,0 +1,29 @@
+
+source("data-raw/1-baixar_arquivos.R", encoding = "UTF-8")
+source("data-raw/2-ler_arquivos.R", encoding = "UTF-8")
+source("data-raw/3-limpar_base.R", encoding = "UTF-8")
+
+
+retorna_foto <- function(falante) {
+
+  falante <- stringr::str_to_upper(falante)
+
+  foto_desconhecido <- "https://queridojeito.com/wp-content/uploads/2016/09/Autor-Desconhecido.jpg"
+
+  endereco_foto <- tabela_fotos %>%
+    dplyr::filter(falante == {{falante}}) %>%
+    dplyr::pull(url_foto)
+
+  if (length(endereco_foto) != 1) {
+
+    endereco_foto <- foto_desconhecido
+
+  }
+
+  return(endereco_foto)
+
+}
+
+source("data-raw/exporta_base_tokenizada.R", encoding = "UTF-8")
+source("data-raw/exporta_fotos.R", encoding = "UTF-8")
+
